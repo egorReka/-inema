a53128320708ea3575d33b7e769299a7c43f422a
egorReka <e.reka55@yandex.ru>
2024-06-01 17:37:51 +0300

    feat: add view modules

---
+++ src/view/film-list-view.js
@@ -0,0 +1,26 @@
+import { createElement } from '../render';
+
+const createFilmListTemplate = () =>
+  `
+  <section class="films-list">
+    <h2 class="films-list__title">There are no movies in our database</h2>
+  </section>
+  `;
+
+export default class FilmListView {
+  getTemplate() {
+    return createFilmListTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
